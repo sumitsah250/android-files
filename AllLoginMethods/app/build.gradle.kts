0f23f0d627c84f0ae344be53e3bf2fcf7efba48b
sumit <sumitsah250@gmail.com>
2024-07-03 18:21:02 +0545

    all login system

--- AllLoginMethods/app/build.gradle.kts
+++ AllLoginMethods/app/build.gradle.kts
@@ -40,4 +40,7 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+    implementation ("com.hbb20:ccp:2.5.0")
+    implementation ("de.hdodenhof:circleimageview:3.1.0")
+    implementation ("com.github.bumptech.glide:glide:4.16.0")
 }
